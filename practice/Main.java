f1beaf1355713f9ce8d78564623266c59ca3c0ab
Ruthvik-02 <ruthvikgowda07@gmail.com>
2023-09-16 22:56:06 +0530

    practice programs done

---
+++ practice/Main.java
@@ -0,0 +1,75 @@
+package practice;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList();
+        a.add(10);
+        a.add(20);
+        a.add(30);
+        a.add(40);
+        a.add(50);
+        a.add(60);
+
+        //System.out.println(a);
+        //System.out.println(a.indexOfValue(60));
+         a.remove(4);
+        System.out.println(a);
+
+    }
+}
+class ArrayList{
+    private int items[];
+
+    private int size;
+
+    private int index = 0;
+
+    ArrayList(){
+        this(5);
+    }
+
+    public ArrayList(int size){
+        items = new int[size];
+    }
+
+    void add(int value){
+        if(index == items.length){
+            resize();
+        }
+        items[index++] = value;
+    }
+
+    int indexOfValue(int value){
+        for(int i=0;i<index;i++) {
+            if(value == items[i])
+                return i;
+        }
+        return -1;
+    }
+
+    private void resize(){
+        int tempArray[] = new int[items.length*2];
+        for(int i=0;i<items.length;i++){
+            tempArray[i]=items[i];
+        }
+        items = tempArray;
+    }
+
+    public void remove(int value){
+        
+        for(int i=value;i<index;i++) {
+                items[i] = items[i+1];
+        }
+        index--;
+    }
+
+    public String toString(){
+        int[] temp2 = new int[index];
+        for(int i=0;i<index;i++){
+            temp2[i] = items[i];
+        }
+        return Arrays.toString(temp2);
+    }
+}
