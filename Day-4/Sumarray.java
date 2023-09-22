6800cc8fc5a7ee2cec707ea52a1073b38148d4d1
Ruthvik-02 <ruthvikgowda07@gmail.com>
2023-09-22 23:35:34 +0530

    Day-4 programs done

---
+++ Day-4/Sumarray.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Sumarray {
+    public static void main(String[] args) {
+         
+        int arr[] = {1,2,3,4,5};
+        int n = arr.length;
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        
+        // int arr[] = new int[n];
+
+        // for (int i = 0; i < n; i++) {
+        //     arr[i]=sc.nextInt();
+        // }
+        int sum=0;
+
+        for (int i = 0; i < n; i++) {
+            sum+=arr[i];
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i]>max) {
+                max = arr[i];
+            }
+            if (arr[i]<min) {
+                min = arr[i];
+            }
+        }
+        // System.out.println(sum-max);
+        // System.out.println(sum-min);
+
+        System.out.println(sum);
+    }
+
+
+}
