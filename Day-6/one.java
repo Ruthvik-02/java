f32d665fec97725798bf6f3e4683adfc862a78ff
Ruthvik-02 <ruthvikgowda07@gmail.com>
2023-10-03 21:21:51 +0530

    Day-9 programs done

--- Day-6/one.java
+++ Day-6/one.java
@@ -3,7 +3,7 @@ public class one {
     public static void main(String[] args) {
         int a[] = {1,2,3,4,5};
 
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length-1; i++) {
             System.out.println(a[i]+" "+a[i+1]);
         }
     }
