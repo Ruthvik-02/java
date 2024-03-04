c3facac2910c03f22ffb4703808a1300c9d98b26
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-03-04 07:51:38 +0530

    leetcode

---
+++ leetcode/ByandSell.java
@@ -0,0 +1,23 @@
+public class ByandSell {
+    public int maxProfit(int[] prices) {
+        int len = prices.length;
+        int maxprofit = 0;
+
+        if(len==1){
+            return maxprofit;
+        }
+
+        int left =0;
+
+        for(int right=1;right<len;right++){
+            if(prices[right]>prices[left]){
+                int currentprofit = prices[right]-prices[left];
+                maxprofit = Math.max(maxprofit,currentprofit);
+            }
+            else{
+                left=right;
+            }
+        }
+        return maxprofit;
+    }
+}
