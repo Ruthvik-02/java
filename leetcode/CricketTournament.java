31d7fa7f8805654c3a2df2ddc128d2fc80b10768
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-03-09 14:22:40 +0530

    leetcode

--- leetcode/CricketTournament.java
+++ leetcode/CricketTournament.java
@@ -1,5 +1,21 @@
 package leetcode;
 
+import java.util.Scanner;
+
 public class CricketTournament {
-    
+    	public static void main (String[] args) throws java.lang.Exception{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-->0){
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    if(n<=m){
+		        System.out.println("No");
+		    }
+		    else{
+		        System.out.println("Yes");   
+		    }
+		}
+	}
 }
