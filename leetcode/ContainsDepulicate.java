5ac10d64c65c4d473f5055d42e01de8c9ca08245
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-03-11 08:08:39 +0530

    leetcode

---
+++ leetcode/ContainsDepulicate.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class ContainsDepulicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1])
+                return true;
+        }
+        return false;
+    }
+}
