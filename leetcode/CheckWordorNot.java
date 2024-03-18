d7186a82266478edfc4e545e77d250e443ece7ca
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-03-18 20:47:36 +0530

    leetcode

---
+++ leetcode/CheckWordorNot.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class CheckWordorNot {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String words[] = sentence.split(" ");
+
+        for(int i=0;i<words.length;i++){
+            if(words[i].startsWith(searchWord)){
+                return i+1;
+            }
+        }
+        return -1;
+    }
+}
