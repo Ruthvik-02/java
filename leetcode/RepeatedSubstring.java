c61447d5068b4d346c770acfdd4fa1e82a0d1559
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-03-25 21:38:08 +0530

    leetcode

---
+++ leetcode/RepeatedSubstring.java
@@ -0,0 +1,17 @@
+public class RepeatedSubstring {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+
+        for(int i=n/2;i>=1;i--){
+            if(n%i==0){
+                String str = s.substring(0,i);
+                StringBuilder sb = new StringBuilder();
+                sb.append(str.repeat(n/i));
+                if(sb.toString().equals(s)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
