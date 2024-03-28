2526cf94118229cd4a79cdc5212151fff45c7443
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-03-28 21:30:07 +0530

    leetcode

--- leetcode/ValidPalindrome.java
+++ leetcode/ValidPalindrome.java
@@ -1,25 +1,18 @@
 package leetcode;
 
 public class ValidPalindrome {
-    public static void main(String[] args) {
-        String s = "A man, a plan, a canal: Panama";
-        //String s = " ";
-
+    public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
-        int i = 0;
+        int i=0;
         int j = s.length()-1;
-        boolean flag = true; 
 
-        while (i<=j) {
-            if (s.charAt(i)!=s.charAt(j)) {
-                System.out.println("false");
-                 flag = false;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
             }
             i++;
             j--;
         }
-        if (flag) {
-            System.out.println("true");
-        }
+    return true;
     }
 }
