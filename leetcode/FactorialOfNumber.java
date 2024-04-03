492aa81b6991147cf7dc5fe80179aa73d8b87ce7
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-04-03 20:14:01 +0530

    leetcode

---
+++ leetcode/FactorialOfNumber.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class FactorialOfNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factOfNumber(n));
+        System.out.println(factorialRec(n));
+    }
+
+    public static int factOfNumber(int n){
+        int fact = 1;
+        for (int i = 1; i <=4 n; i++) {
+            fact*=i;
+        }
+        return fact;
+    }
+
+    public static int factorialRec(int n){
+        if (n==1) {
+            return 1;
+        }
+
+        return n*factorialRec(n-1);
+    }
+}
