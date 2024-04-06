a7ab5a4056b02206570215cc5f31259e8be85a73
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-04-06 18:49:35 +0530

    practicerecu

---
+++ practicerecu/FibonacciSeries.java
@@ -0,0 +1,23 @@
+package practicerecu;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println(fibonacci(i));
+        }
+    }
+
+    public static int fibonacci(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+        int fnum = fibonacci(n-1);
+        int snum = fibonacci(n-2);
+        return fnum+snum;
+    }
+}
