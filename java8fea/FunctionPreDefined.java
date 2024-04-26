d8419d37ed0a286bbccbbbaf21d9d2e26dd31021
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-04-26 17:08:29 +0530

    java8fea

--- java8fea/FunctionPreDefined.java
+++ java8fea/FunctionPreDefined.java
@@ -3,13 +3,16 @@ package java8fea;
 import java.util.function.Function;
 public class FunctionPreDefined {
     public static void main(String[] args) {
+
+        //function -> filter(2 parameter) calculations
+
         Function<Integer, Integer> square = (num) -> num * num;
 
-    System.out.println(square.apply(5));
+        System.out.println(square.apply(5));
 
-    Function<String, Integer> len = (str) -> str.length();
+        Function<String, Integer> len = (str) -> str.length();
 
-    System.out.println(len.apply("abcd"));
+        System.out.println(len.apply("abcd"));
     }
 
     
