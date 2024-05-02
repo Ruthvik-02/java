e4efdd7653212e627b237c55eec3e4642a50a401
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-05-02 20:08:04 +0530

    practicerecu

---
+++ practicerecu/MaxSpan.java
@@ -0,0 +1,31 @@
+package practicerecu;
+
+public class MaxSpan {
+    public static void main(String[] args) {
+        int arr[] = {1,2,1,1,3};
+
+        System.out.println(maxSpan(arr));
+    }
+    public static int maxSpan(int[] nums) {
+        int max = 0;
+        
+        for(int i=0;i<nums.length;i++){
+          int cur = nums[i];
+          int index = i;
+          
+          for(int j=i+1;j<nums.length;j++){
+            if(nums[j]==cur) {
+              index = j;
+            }
+          }
+          
+            int element = index-i+1;
+        
+            if(element > max) {
+            max = element;
+            }
+        }
+        
+        return max;
+      }
+}
