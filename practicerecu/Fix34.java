2e87e3227fa94a881925c4f542fc335916d64c7e
Ruthvik-02 <ruthvikgowda07@gmail.com>
2024-05-04 18:35:03 +0530

    practicerecu

---
+++ practicerecu/Fix34.java
@@ -0,0 +1,32 @@
+package practicerecu;
+
+public class Fix34 {
+    public static void main(String[] args) {
+
+        int nums[] = {1, 3, 1, 4, 4, 3, 1};
+        int res[] = fix34(nums);
+
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i]+" ");
+        }
+    }
+
+    public static int[] fix34(int[] nums) {
+        int n=0;
+        for(int i=0;i<nums.length;i++){
+          if(nums[i]==3){
+              for(int j=n;j<nums.length;j++){
+                  if(nums[j]==4){
+                      int temp = nums[j];
+                      nums[j] = nums[i+1];
+                      nums[i+1]=temp;
+                      
+                      n=j;
+                      break;
+                  }
+              }
+          }
+        }
+        return nums;
+      }
+}
